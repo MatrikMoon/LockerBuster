4d18cecdb6835bd0a0bc7b4f174355675efded66
Moon <5924350+MatrikMoon@users.noreply.github.com>
2021-05-11 03:36:48 -0500

    Respect folder structure when copying files

--- LockerBuster/MainWindow.xaml.cs
+++ LockerBuster/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Forms;
 
@@ -12,8 +13,23 @@ namespace LockerBuster
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string TargetFolder { get; set; }
-        private string[] TargetFiles { get; set; }
+        private string[] _targetFiles;
+        private string[] TargetFiles {
+            get => _targetFiles;
+            set {
+                //Add folders and files that end in the pl2 or vl2, or are directories
+                var filesAndFolders = new List<string>();
+                filesAndFolders.AddRange(
+                    value.Where(x => 
+                            x.EndsWith(".pl2") ||
+                            x.EndsWith(".vl2") ||
+                            File.GetAttributes(x).HasFlag(FileAttributes.Directory)
+                        )
+                    );
+                _targetFiles = filesAndFolders.ToArray();
+            }
+        }
+
         private string DestinationFolder { get; set; }
 
         public MainWindow()
@@ -21,27 +37,26 @@ namespace LockerBuster
             InitializeComponent();
         }
 
-        private string[] GetSelectedFiles()
-        {
-            List<string> files = new List<string>();
-
-            if (!string.IsNullOrEmpty(TargetFolder))
-            {
-                files.AddRange(Directory.EnumerateFiles(TargetFolder, "*.*", SearchOption.AllDirectories)
-                    .Where(x => x.EndsWith(".pl2") || x.EndsWith(".vl2")));
-            }
-            else if (TargetFiles.Length > 0)
-            {
-                files.AddRange(TargetFiles.Where(x => x.EndsWith(".pl2") || x.EndsWith(".vl2")));
-            }
-
-            return files.ToArray();
-        }
-
         private void RefreshFileList()
         {
             FileList.Items.Clear();
-            foreach (var file in GetSelectedFiles()) FileList.Items.Add(file);
+            foreach (var file in TargetFiles ?? new string[] { }) FileList.Items.Add(file);
+        }
+
+        private int GetTotalFileCount() {
+            var counter = 0;
+            foreach (var item in TargetFiles)
+            {
+                if (File.GetAttributes(item).HasFlag(FileAttributes.Directory))
+                {
+                    counter += Directory.EnumerateFiles(item, "*.*", SearchOption.AllDirectories).Where(x => x.EndsWith(".pl2") || x.EndsWith(".vl2")).Count();
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+            return counter;
         }
 
         private void TargetFolderButton_Click(object sender, RoutedEventArgs e)
@@ -53,7 +68,7 @@ namespace LockerBuster
             };
             if (dialog.Show())
             {
-                TargetFolder = dialog.FileName;
+                TargetFiles = new[] { dialog.FileName };
                 RefreshFileList();
             }
         }
@@ -86,25 +101,82 @@ namespace LockerBuster
 
         private void DecryptButton_Click(object sender, RoutedEventArgs e)
         {
-            foreach (var file in GetSelectedFiles())
+            DestinationFolderButton.IsEnabled = false;
+            TargetFilesButton.IsEnabled = false;
+            DecryptionProgressBar.IsEnabled = true;
+
+            DecryptionProgressBar.Value = 0;
+            DecryptionProgressBar.Maximum = GetTotalFileCount();
+
+            Task.Run(() =>
             {
-                Decrypt(file, DestinationFolder);
-                FileList.Items.Remove(file);
-            }
+                foreach (var file in TargetFiles)
+                {
+                    if (File.GetAttributes(file).HasFlag(FileAttributes.Directory))
+                    {
+                        DirectoryDecrypt(file, DestinationFolder);
+                    }
+                    else
+                    {
+                        FileDecrypt(file, DestinationFolder);
+                        Dispatcher.Invoke(() => DecryptionProgressBar.Value++);
+                    }
+                    Dispatcher.Invoke(() => FileList.Items.Remove(file));
+                }
+            });
+
+            DestinationFolderButton.IsEnabled = true;
+            TargetFilesButton.IsEnabled = true;
+            DecryptionProgressBar.IsEnabled = false;
         }
 
-        private void Decrypt(string filePath, string destination)
+        private void FileDecrypt(string filePath, string destination)
         {
-            var encryptedBytes = File.ReadAllBytes(filePath);
-            var byteLength = Math.Min(encryptedBytes.Length, 2048);
+            var fileBytes = File.ReadAllBytes(filePath);
+            var byteLength = Math.Min(fileBytes.Length, 2048);
             byte[] encrypted = new byte[byteLength];
 
-            Array.Copy(encryptedBytes, 0, encrypted, 0, byteLength);
+            Array.Copy(fileBytes, 0, encrypted, 0, byteLength);
             byte[] decrypted = XOROperations.Decrypt(encrypted);
 
-            Array.Copy(decrypted, 0, encryptedBytes, 0, byteLength);
+            Array.Copy(decrypted, 0, fileBytes, 0, byteLength);
 
-            File.WriteAllBytes($"{destination}/{Path.GetFileNameWithoutExtension(filePath)}", encryptedBytes);
+            File.WriteAllBytes($"{destination}/{Path.GetFileNameWithoutExtension(filePath)}", fileBytes);
+        }
+
+        private void DirectoryDecrypt(string sourceDirName, string destDirName)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(destDirName);
+
+            // Decrypt files to new destination, but only .pl2 and .vl2
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                if (file.FullName.EndsWith(".pl2") || file.FullName.EndsWith(".vl2"))
+                {
+                    FileDecrypt(file.FullName, destDirName);
+                    Dispatcher.Invoke(() => DecryptionProgressBar.Value++);
+                }
+            }
+
+            // Copy subdirectories
+            foreach (DirectoryInfo subdir in dirs)
+            {
+                DirectoryDecrypt(subdir.FullName, Path.Combine(destDirName, subdir.Name));
+            }
         }
     }
 }
